793c69509f0f1e966d54c901014baee476a41b42
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-01 12:47:33 -0500

    added html en tag for seo improvment

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Html lang='en'></Html>
         <Head>
           <link
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap'
