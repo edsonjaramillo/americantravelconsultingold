b333cb0c714d563637f2da2a207ff26f10320dd6
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-01 13:42:39 -0500

    moved viewport from _document to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/main.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        ></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -15,10 +15,6 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap'
             rel='stylesheet'
           />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1.0'
-          ></meta>
         </Head>
         <body>
           <Main />
