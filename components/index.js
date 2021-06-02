056331baef6140cdc75b99498cff41198b3a312b
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-02 15:16:32 -0500

    file for components export

---
+++ components/index.js
@@ -0,0 +1,14 @@
+// Navigation Bar Components
+export { default as Navbar } from '@/components/shared/NavigationBar/Navbar';
+export { default as Hamburger } from '@/components/shared/NavigationBar/Hamburger';
+
+// Layout Components
+export { default as Section } from '@/components/shared/Layout/Section';
+
+// Home Components
+export { default as TripType } from '@/components/home/TripType';
+export { default as Location } from '@/components/home/Location';
+export { default as Testimonial } from '@/components/home/Testimonial';
+
+// Contact Components
+export { default as RequestForm } from '@/components/contact/RequestForm';
