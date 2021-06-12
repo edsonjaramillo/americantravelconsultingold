735fe6bc3b03ec428f7e9e0c21e73a5491c84bbc
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-11 20:31:39 -0500

    added components to imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import '@/styles/main.css';
 import Head from 'next/head';
-import { Navbar } from '@/components/index';
+import { Navbar, MobileFooter } from '@/components/index';
+import { useState } from 'react';
+import '@/styles/main.css';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,13 +9,13 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1.0'
-        ></meta>
+          content='width=device-width, initial-scale=1.0'></meta>
       </Head>
       <Navbar />
       <main>
         <Component {...pageProps} />
       </main>
+      <MobileFooter />
     </>
   );
 }
