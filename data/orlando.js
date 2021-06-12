306d4ece05fab6b6d6c57d68db488a7c22ff182c
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-11 20:30:34 -0500

    orlando coords added

---
+++ data/orlando.js
@@ -0,0 +1,69 @@
+const Data = [
+  {
+    name: 'Andretti Indoor Karting',
+    lat: 28.434731238255473,
+    lon: -81.46423458843692,
+  },
+  {
+    name: 'Disney Springs',
+    lat: 28.371273458120697,
+    lon: -81.51909875146806,
+  },
+  {
+    name: 'Disney World',
+    lat: 28.385233,
+    lon: -81.563345873,
+  },
+  {
+    name: 'Fun Spot America',
+    lat: 28.468053559900238,
+    lon: -81.45560835520892,
+  },
+  {
+    name: 'Islands of Adventures',
+    lat: 28.472030606213387,
+    lon: -81.47092028843626,
+  },
+  {
+    name: 'Kennedy Space Center',
+    lat: 28.471641929829243,
+    lon: -81.32543748534415,
+  },
+  {
+    name: 'LEGOLAND',
+    lat: 27.989977286258956,
+    lon: -81.68881482646728,
+  },
+  {
+    name: 'Madame Tussauds',
+    lat: 28.44393238895671,
+    lon: -81.46859497309316,
+  },
+  {
+    name: 'Medieval Times',
+    lat: 28.31828214253943,
+    lon: -81.42960272493795,
+  },
+  {
+    name: 'SeaWorld Orlando',
+    lat: 28.410091116062684,
+    lon: -81.4594011112256,
+  },
+  {
+    name: 'Universal City Walk',
+    lat: 28.473164014932195,
+    lon: -81.46596645765283,
+  },
+  {
+    name: 'Universal Studios',
+    lat: 28.47521,
+    lon: -81.47493,
+  },
+  {
+    name: 'Wheel at ICON Park',
+    lat: 28.44389457423092,
+    lon: -81.4682518289136,
+  },
+];
+
+export default Data;
