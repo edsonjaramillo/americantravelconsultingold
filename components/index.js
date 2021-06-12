2ab5504c2e78f3ef24058c2876e36dede1bdfa8d
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-11 20:25:28 -0500

    added  mobile footer and map component

--- components/index.js
+++ components/index.js
@@ -2,6 +2,9 @@
 export { default as Navbar } from '@/components/shared/NavigationBar/Navbar';
 export { default as Hamburger } from '@/components/shared/NavigationBar/Hamburger';
 
+// Footer Components
+export { default as MobileFooter } from '@/components/shared/Footer/MobileFooter';
+
 // Layout Components
 export { default as Section } from '@/components/shared/Layout/Section';
 
@@ -13,3 +16,6 @@ export { default as Testimonial } from '@/components/home/Testimonial';
 // Contact Components
 export { default as RequestForm } from '@/components/contact/RequestForm';
 export { default as Input } from '@/components/contact/Input';
+
+// Destination Components
+export { default as Map } from '@/components/destinations/Map';
