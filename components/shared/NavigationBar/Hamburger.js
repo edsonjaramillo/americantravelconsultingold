a4ef4e40c9d3047e2ded44aa334bb7cf60d2becd
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-13 13:36:07 -0500

    cleaned up functions

--- components/shared/NavigationBar/Hamburger.js
+++ components/shared/NavigationBar/Hamburger.js
@@ -1,23 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function Hamburger() {
-  useEffect(() => {
+  const openMenu = () => {
     const button = document.getElementById('hamburger');
-    button.addEventListener('click', () => {
-      const burger = document.querySelector('.navbar__hamburger');
-      const nav = document.querySelector('.navbar__items');
-      burger.classList.toggle('menutoggle');
-      nav.classList.toggle('open');
-    });
-  }, []);
+    const burger = document.querySelector('.navbar__hamburger');
+    const nav = document.querySelector('.navbar__items');
+    burger.classList.toggle('menutoggle');
+    nav.classList.toggle('open');
+  };
 
   return (
     <>
-      <section className='navbar__hamburger' id='hamburger'>
+      <div className='navbar__hamburger' id='hamburger' onClick={openMenu}>
         <div className='navbar__hb1'></div>
         <div className='navbar__hb2'></div>
         <div className='navbar__hb3'></div>
-      </section>
+      </div>
     </>
   );
 }
