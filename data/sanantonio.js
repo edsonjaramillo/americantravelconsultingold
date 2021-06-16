c7c928e4eb68b0d2a0acf97f8678770ccb0d3e55
Edson Jaramillo <edsonajaramillo@gmail.com>
2021-06-15 20:58:39 -0500

    added coords

---
+++ data/sanantonio.js
@@ -0,0 +1,59 @@
+const Data = [
+  {
+    name: 'Alamo',
+    lat: 29.426040201253915,
+    lon: -98.48611665037856,
+  },
+  {
+    name: 'LEGOLAND',
+    lat: 29.423469806882046,
+    lon: -98.48583446018608,
+  },
+  {
+    name: 'River Tours',
+    lat: 29.423250765979322,
+    lon: -98.48481091153612,
+  },
+  {
+    name: 'San Antonio Aquarium',
+    lat: 29.4871387384517,
+    lon: -98.60973158901923,
+  },
+  {
+    name: 'San Antonio Botanical Garden',
+    lat: 29.458099807396618,
+    lon: -98.45869639087167,
+  },
+  {
+    name: 'San Antonio Museum of Art',
+    lat: 29.43725739474263,
+    lon: -98.48214688902137,
+  },
+  {
+    name: 'San Antonio Zoo',
+    lat: 29.46239972346665,
+    lon: -98.47379830621499,
+  },
+  {
+    name: 'San Fernando Cathedral',
+    lat: 29.4244858858715,
+    lon: -98.49422111785779,
+  },
+  {
+    name: 'SeaWorld',
+    lat: 29.456382640177896,
+    lon: -98.69995813319998,
+  },
+  {
+    name: 'Six Flags Fiesta Texas',
+    lat: 29.59912491079622,
+    lon: -98.61057143319387,
+  },
+  {
+    name: 'Tower of the Americas',
+    lat: 29.418957553863777,
+    lon: -98.48350874707191,
+  },
+];
+
+export default Data;
